894ffa00a5c232fbfd9ad1408fc8da8de8d39cdd
andyl7an <atan12@calpoly.edu>
2017-05-06 16:03:04 -0700

    UserAccount base

--- app/src/main/java/UserAccount.java
+++ app/src/main/java/UserAccount.java
@@ -1,56 +1,202 @@
 /**
- * Created by imeanigeese on 5/5/2017.
+ * The User Account class contains all variable that define a individual on the Swapacado platform.
+ *
+ *@author Andy Tan
+ *@version 1.0
+ *@since 2017-05-06
  */
 
 public class UserAccount {
-    protected String name;
+    protected String username;
+    protected String firstName;
+    protected String lastName;
     protected int id;
     protected String phoneNumber;
     protected double rating;
-    protected String imagepath;
+    protected String imagePath;
 
-    //constructor
-    public UserAccount(){
+    /**
+     * Constructor for new user account
+     */
+    public UserAccount()
+    {
 
     }
 
-    public String getName(){
-        return this.name;
+    /**
+     * This method returns the user's username
+     * @return this.username
+     */
+    public String getUsername()
+    {
+        return this.username;
     }
 
-    public int getID(){
+    /**
+     * This method returns the user's first name
+     * @return this.firstName
+     */
+    public String getFirstname()
+    {
+        return this.firstName;
+    }
+
+    /**
+     * This method returns the user's last name
+     * @return this.Lastname
+     */
+    public String getLastname()
+    {
+        return this.lastName;
+    }
+
+    /**
+     * This method returns the user's unique id
+     * @return this.id
+     */
+    public int getID()
+    {
         return this.id;
     }
 
-    public String getPhoneNumber(){
+    /**
+     * This method returns the user's phone number
+     * @return this.phoneNumber
+     */
+    public String getPhoneNumber()
+    {
         return this.phoneNumber;
     }
 
-    public double getRating(){
+    /**
+     * This method returns the user's rating
+     * @return this.rating
+     */
+    public double getRating()
+    {
         return this.rating;
     }
 
-    public String getImagepath(){
-        return this.imagepath;
+    /**
+     * This method returns the user's image path
+     * @return this.imagepath
+     */
+    public String getImagepath()
+    {
+        return this.imagePath;
     }
 
-    public boolean setName(){
+    /**
+     * This method returns the user's unique id
+     * @param input username of user
+     * @return true if successfully set, false otherwise
+     */
+    public boolean setUsername(String input)
+    {
+        int userLen = input.length();
+        if (userLen > 20 || userLen < 4)
+        {
+            return false;
+        }
+        this.username = input;
         return true;
     }
 
-    public boolean setId(){
+    /**
+     * This method returns the user's unique id
+     * @param input first name of user
+     * @return true if successfully set, false otherwise
+     */
+    public boolean setFirstname(String input)
+    {
+        for (int i = 0; i < input.length(); i++)
+        {
+            char c = input.charAt(i);
+            if (Character.isDigit(c))
+            {
+                return false;
+            }
+        }
+        this.firstName = input;
         return true;
     }
 
-    public boolean setPhoneNumber(){
+    /**
+     * This method returns the user's unique id
+     * @param input last name of user
+     * @return true if successfully set, false otherwise
+     */
+    public boolean setLastname(String input)
+    {
+        for (int i = 0; i < input.length(); i++)
+        {
+            char c = input.charAt(i);
+            if (Character.isDigit(c))
+            {
+                return false;
+            }
+        }
+        this.lastName = input;
         return true;
     }
 
-    public boolean setRating(){
+    /**
+     * This method returns the user's unique id
+     * @param input id of user
+     * @return true if successfully set, false otherwise
+     */
+    public boolean setId(int input)
+    {
+        this.id = input;
         return true;
     }
 
-    public boolean setImagePath(){
+    /**
+     * This method returns the user's unique id
+     * @param input phone number of user
+     * @return true if successfully set, false otherwise
+     */
+    public boolean setPhoneNumber(String input)
+    {
+        if (input.length() != 10)
+        {
+            return false;
+        }
+        for (int i = 0 ; i < input.length(); i++)
+        {
+            char c = input.charAt(i);
+            if (!Character.isDigit(c))
+            {
+                return false;
+            }
+        }
+        this.phoneNumber = input;
+        return true;
+    }
+
+    /**
+     * This method returns the user's unique id
+     * @param input rating of user
+     * @return true if successfully set, false otherwise
+     */
+    public boolean setRating(float input)
+    {
+        if (input > 5 || input < 0)
+        {
+            return false;
+        }
+        this.rating = input;
+        return true;
+    }
+
+    /**
+     * This method returns the user's unique id
+     * @param input image path to profile picture of user
+     * @return true if successfully set, false otherwise
+     */
+    public boolean setImagePath(String input)
+    {
+        this.imagePath = input;
         return true;
     }
 
