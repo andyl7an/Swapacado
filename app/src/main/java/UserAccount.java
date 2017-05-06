42f09c2b6a8abec24494eaad40d9f4995ff75536
andyl7an <atan12@calpoly.edu>
2017-05-05 17:47:55 -0700

    UserAccount class base

--- app/src/main/java/UserAccount.java
+++ app/src/main/java/UserAccount.java
@@ -1,8 +1,57 @@
-package PACKAGE_NAME;
-
 /**
  * Created by imeanigeese on 5/5/2017.
  */
 
 public class UserAccount {
+    protected String name;
+    protected int id;
+    protected String phoneNumber;
+    protected double rating;
+    protected String imagepath;
+
+    //constructor
+    public UserAccount(){
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public double getRating(){
+        return this.rating;
+    }
+
+    public String getImagepath(){
+        return this.imagepath;
+    }
+
+    public boolean setName(){
+        return true;
+    }
+
+    public boolean setId(){
+        return true;
+    }
+
+    public boolean setPhoneNumber(){
+        return true;
+    }
+
+    public boolean setRating(){
+        return true;
+    }
+
+    public boolean setImagePath(){
+        return true;
+    }
+
 }
