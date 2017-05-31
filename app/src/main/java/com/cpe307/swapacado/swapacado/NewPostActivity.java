d8e9daf61d6f96a3aac4c4247f46c84577071bb0
AalokAhluwalia <aalokahluwalia@gmail.com>
2017-05-30 22:39:11 -0700

    Stub for new post

--- app/src/main/java/com/cpe307/swapacado/swapacado/NewPostActivity.java
+++ app/src/main/java/com/cpe307/swapacado/swapacado/NewPostActivity.java
@@ -1,7 +1,11 @@
 package com.cpe307.swapacado.swapacado;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class NewPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        ViewGroup injectionLocation = (LinearLayout) this.findViewById(R.id.newPost_customRowInjectionLocation);
+        LayoutInflater myInflater = null;
+        View child = getLayoutInflater().inflate(R.layout.custom_row, null);
+        injectionLocation.addView(child);
+//        myInflater.inflate;
+//        View.inflate(this.getApplicationContext(),R.id.xml_customRow, injectionLocation);
+
     }
+    //On click for the post button
 }
