865fe68368825a47e234f68c085336909bdffbc9
AalokAhluwalia <aalokahluwalia@gmail.com>
2017-06-02 13:52:41 -0700

    Adding changes for contact popup

--- app/src/main/java/com/cpe307/swapacado/swapacado/PostAdapter.java
+++ app/src/main/java/com/cpe307/swapacado/swapacado/PostAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,6 +38,7 @@ class CustomAdapter extends ArrayAdapter<Post>{
         ImageView faceLocation = (ImageView) theActualView.findViewById(R.id.postCard_egg);
         faceLocation.setImageResource(singlePost.getFaceRID());
 
+
         String distanceString = singlePost.getDistanceString();
         TextView distanceView = (TextView) theActualView.findViewById(R.id.postCard_distance);
         distanceView.setText(distanceString);
@@ -52,6 +54,16 @@ class CustomAdapter extends ArrayAdapter<Post>{
         String wantString = singlePost.getWantString();
         TextView wantView = (TextView) theActualView.findViewById(R.id.postCard_want);
         wantView.setText(wantString);
+
+        Button contactButton = (Button)  theActualView.findViewById(R.id.postCard_contact);
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater myInflater = LayoutInflater.from(view.getContext());
+                View contact_popup = myInflater.inflate(R.layout.contact_layout, R.id.home_listview, false);
+                ViewGroup injectLocation = (ViewGroup);
+            }
+        });
 
         //Set profile picture of user
         //Show rating
